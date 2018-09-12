40a80aa7faa820d3162daf886f761d6a9caac277
Robert Hedman <robert.hedman@mac.com>
2018-09-12 09:39:32 +0200

    dont try to remove nonexisting dirs

--- UpdatePC.sh
+++ UpdatePC.sh
@@ -8,9 +8,15 @@ wstool init src permo_car_pc.rosinstall
 wstool update -t src
 
 # clean up nodes not needed in remote repo
+if [ -d "src/PadPub/wiimote" ]; then
 rm -r src/PadPub/ps3joy
+fi
+if [ -d "src/PadPub/wiimote" ]; then
 rm -r src/PadPub/spacenav_node
+fi
+if [ -d "src/PadPub/wiimote" ]; then
 rm -r src/PadPub/wiimote
+fi
 
 catkin build
 rm permo_car_pc.rosinstall
