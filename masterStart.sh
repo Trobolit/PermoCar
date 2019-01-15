1df9834bfd35feb93d3a6e905005787a802f3e71
Robert Hedman <robert.hedman@mac.com>
2019-01-15 13:17:29 +0100

    master start upp and runinng

---
+++ masterStart.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function trap_ctrlc()
+{
+	ssh ubuntu@10.9.0.3 "pkill -9 startWscream.sh"
+	ssh ubuntu@10.9.0.3 "pkill -9 rosmaster"
+	ssh ubuntu@10.9.0.3 "pkill -9 scream_sender"
+	ssh ubuntu@10.9.0.3 "pkill -9 roslaunch"
+	ssh ubuntu@10.9.0.3 "pkill -9 rtsp"
+	exit 2
+}
+trap "trap_ctrlc" 2
+
+source ~/catkin_ws/devel/setup.bash
+
+( ssh -t ubuntu@10.9.0.3 '~/PermoCar/startWscream.sh && bash' ) &
+P0=$!
+
+sleep 10
+
+export ROS_MASTER_URI="http://10.9.0.3:11311" # Probably not needed
+export ROS_IP="10.9.0.2"
+cd ~/scream
+( ./screamlaptop.sh &> screamlaplog.log ) &
+P1=$!
+cd ~/PermoCar
+roslaunch launch_teleop pc_launch.launch
+
+wait $P0 $P1
